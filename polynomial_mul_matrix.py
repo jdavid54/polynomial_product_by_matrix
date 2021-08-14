be8e7d0bcadfe0393bc8e66fe0c8811a93dd1958
Jean David <jeandavid54@aol.com>
2021-08-14 21:08:51 +0200

    Add files via upload

--- polynomial_mul_matrix.py
+++ polynomial_mul_matrix.py
@@ -20,31 +20,31 @@ def polymul_bymatrix(P1, P2):
     if deg_max > m: # add zero only if deg_max is greater than number of coefficients of P1
         P1.extend([0]*(n-1))
     print('\nP1\n',P1)
-    # create the matrix with the rotating coefficients of P1
-    a = []
+    # create the matrix M with the rotating coefficients of P1
+    M = []
     for k in range(n): 
-        a.append(rotate(P1,-k))
-    a = np.array(a).T
+        M.append(rotate(P1,-k))
+    M = np.array(M).T
     if deg_max == m :
-        a = np.tril(a)  # one case when deg_max = m as no zero was added
-    print('Matrix\n',a)
+        M = np.tril(M)  # one case when deg_max = m as no zero was added
+    print('Matrix\n', M)
     #print('P2\n',P2)
-    b = np.array(P2).reshape(n,1)
-    print('P2 in column\n',b)
-    c = list(np.dot(a,b).T.flatten())
-    return a,b,c
+    B = np.array(P2).reshape(n,1)
+    print('P2 in column\n',B)
+    result = list(np.dot(M, B).T.flatten())
+    return M, B, result
 
 A = [1,5,3,4,5,1,5,6,7]
 B = [1,1]
 matrix, b, c = polymul_bymatrix(A, B)
-print(c)
+print('Result',c)
 
 C = [0,0,0,0,0,0,1]
 
 matrix, b, c = polymul_bymatrix(A, C)
-print(c)
+print('Result',c)
 
 A = [1,5,3]
 B = [1,3]
 matrix, b, c = polymul_bymatrix(A, B)
-print(c)
+print('Result',c)
