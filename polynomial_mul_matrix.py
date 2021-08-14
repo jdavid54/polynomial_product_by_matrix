c3bdde177c709bbfcda1c5f1bf388ddd109d4ca4
Jean David <jeandavid54@aol.com>
2021-08-14 20:53:47 +0200

    Add files via upload

--- polynomial_mul_matrix.py
+++ polynomial_mul_matrix.py
@@ -14,28 +14,30 @@ def make_mat(P1, P2):
     deg_max = deg1 + deg2
     # P1 must have bigger degree than P2
     if m < n:
-        P1,P2 = P2,P1
+        P1,P2 = P2,P1 # swap
         m=len(P1)
         n=len(P2)
     if deg_max > m: # add zero only if final max degree is more than number of coefficients of P1
         P1.extend([0]*(n-1))
-    print('P1\n',P1)
+    print('\nP1\n',P1)
     a = []
     for k in range(n): 
         a.append(rotate(P1,-k))
     a = np.array(a).T
-    if deg_max <= m :
-        a = np.tril(a)
+    if deg_max == m :
+        a = np.tril(a)  # one case when deg_max = m
     print('Matrix\n',a)
-    print('P2\n',P2)
-    b = np.array(P2).reshape(n,1)    
-    c = np.dot(a,b).T 
+    #print('P2\n',P2)
+    b = np.array(P2).reshape(n,1)
+    print('P2 in column\n',b)
+    c = list(np.dot(a,b).T.flatten())
     return a,b,c
 
 A = [1,5,3,4,5,1,5,6,7]
 B = [1,1]
 matrix, b, c = make_mat(A, B)
 print(c)
+
 C = [0,0,0,0,0,0,1]
 
 matrix, b, c = make_mat(A, C)
