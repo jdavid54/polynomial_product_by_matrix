fbcc193b39b70a1b848a37aebf3dabafeaab2f1e
Jean David <jeandavid54@aol.com>
2021-08-14 20:59:07 +0200

    Add files via upload

--- polynomial_mul_matrix.py
+++ polynomial_mul_matrix.py
@@ -3,7 +3,7 @@ import numpy as np
 def rotate(l, n):
     return l[n:] + l[:n]
 
-def make_mat(P1, P2):
+def polymul_bymatrix(P1, P2):
     # get rid of zero end trail
     #P1 =  np.trim_zeros(P1, trim='b')
     #P2 =  np.trim_zeros(P2, trim='b')
@@ -17,15 +17,16 @@ def make_mat(P1, P2):
         P1,P2 = P2,P1 # swap
         m=len(P1)
         n=len(P2)
-    if deg_max > m: # add zero only if final max degree is more than number of coefficients of P1
+    if deg_max > m: # add zero only if deg_max is greater than number of coefficients of P1
         P1.extend([0]*(n-1))
     print('\nP1\n',P1)
+    # create the matrix with the rotating coefficients of P1
     a = []
     for k in range(n): 
         a.append(rotate(P1,-k))
     a = np.array(a).T
     if deg_max == m :
-        a = np.tril(a)  # one case when deg_max = m
+        a = np.tril(a)  # one case when deg_max = m as no zero was added
     print('Matrix\n',a)
     #print('P2\n',P2)
     b = np.array(P2).reshape(n,1)
@@ -35,15 +36,15 @@ def make_mat(P1, P2):
 
 A = [1,5,3,4,5,1,5,6,7]
 B = [1,1]
-matrix, b, c = make_mat(A, B)
+matrix, b, c = polymul_bymatrix(A, B)
 print(c)
 
 C = [0,0,0,0,0,0,1]
 
-matrix, b, c = make_mat(A, C)
+matrix, b, c = polymul_bymatrix(A, C)
 print(c)
 
 A = [1,5,3]
 B = [1,3]
-matrix, b, c = make_mat(A, B)
+matrix, b, c = polymul_bymatrix(A, B)
 print(c)
